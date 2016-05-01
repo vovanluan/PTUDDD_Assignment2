e2439de29f39b6d8fd39ffac15725928721e3cf4
Luan <vovanluan201294@gmail.com>
2016-05-01 17:41:27 +0700

    Update button style

--- app/src/main/java/com/example/luan/activity/LoginActivity.java
+++ app/src/main/java/com/example/luan/activity/LoginActivity.java
@@ -71,7 +71,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
     }
