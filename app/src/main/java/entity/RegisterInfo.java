12aeeba9710de2fd8091a7a6c203c17fcdb58ab2
Bach Do <nguyenbach2810@gmail.cm>
2016-07-28 18:51:02 +0900

    + Add new entity for register with name

---
+++ app/src/main/java/entity/RegisterInfo.java
@@ -0,0 +1,50 @@
+package entity;
+
+/**
+ * Created by Bach Do on 7/27/2016.
+ */
+public class RegisterInfo {
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    public RegisterInfo() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
